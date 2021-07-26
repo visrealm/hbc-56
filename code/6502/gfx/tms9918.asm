665de1186e5031eb50305b08aeadef2c3d969f78
Troy Schrapel <troy.schrapel@petrosys.com.au>
2021-07-27 09:24:54 +0930

    TMS9918 Sprite macros

--- code/6502/gfx/tms9918.asm
+++ code/6502/gfx/tms9918.asm
@@ -80,7 +80,7 @@ TMS_WHITE               = $0f
 ; -----------------------------------------------------------------------------
 TMS_REGISTER_DATA:
 !byte TMS_R0_EXT_VDP_DISABLE | TMS_R0_MODE_GRAPHICS_I
-!byte TMS_R1_RAM_16K | TMS_R1_DISP_ACTIVE | TMS_R1_MODE_GRAPHICS_I
+!byte TMS_R1_RAM_16K | TMS_R1_DISP_ACTIVE | TMS_R1_MODE_GRAPHICS_I | TMS_R1_SPRITE_MAG2
 !byte TMS_VRAM_BASE_ADDRESS >> 10
 !byte TMS_VRAM_COLOR_ADDRESS >> 6
 !byte TMS_VRAM_FONT_ADDRESS >> 11
@@ -313,6 +313,76 @@ tmsInitSpriteTable:
 
         rts
 
+; -----------------------------------------------------------------------------
+; tmsCreateSpritePattern: Create a sprite pattern (.spriteDataAddr is 8 bytes)
+; -----------------------------------------------------------------------------
+!macro tmsCreateSpritePattern .pattInd, .spriteDataAddr {
+
+        ; sprite pattern table
+        +tmsSetAddress TMS_VRAM_SPRITE_PATT_ADDRESS + .pattInd * 8
+
+        ldx #0
+-
+        lda .spriteDataAddr,x
+        sta TMS9918_RAM
+        +tmsWait
+        inx
+        cpx #8
+        bne -
+}
+
+; -----------------------------------------------------------------------------
+; tmsCreateSprite: Create a sprite
+; -----------------------------------------------------------------------------
+!macro tmsCreateSprite .ind, .pattInd, .xPos, .yPos, .color {
+
+        ; sprite attr table
+        +tmsSetAddress TMS_VRAM_SPRITE_ATTR_ADDRESS + .ind * 4
+
+        lda #.yPos
+        sta TMS9918_RAM
+        +tmsWait
+        lda #.xPos
+        sta TMS9918_RAM
+        +tmsWait
+        lda #.pattInd
+        sta TMS9918_RAM
+        +tmsWait
+        lda #.color
+        sta TMS9918_RAM
+        +tmsWait
+}
+
+; -----------------------------------------------------------------------------
+; tmsSpritePos: Set a sprite position
+; -----------------------------------------------------------------------------
+!macro tmsSpritePos .ind, .xPos, .yPos {
+
+        ; sprite attr table
+        +tmsSetAddress TMS_VRAM_SPRITE_ATTR_ADDRESS + .ind * 4
+
+        lda #.yPos
+        sta TMS9918_RAM
+        +tmsWait
+        lda #.xPos
+        sta TMS9918_RAM
+        +tmsWait
+}
+
+
+; -----------------------------------------------------------------------------
+; tmsSpriteColor: Change a sprite color
+; -----------------------------------------------------------------------------
+!macro tmsSpriteColor .ind, .color {
+
+        ; sprite attr table
+        +tmsSetAddress TMS_VRAM_SPRITE_ATTR_ADDRESS + (.ind * 4) + 3
+
+        lda #.color
+        sta TMS9918_RAM
+        +tmsWait
+}
+
 
 ; -----------------------------------------------------------------------------
 ; tmsPrint: Print immediate text
@@ -356,103 +426,3 @@ tmsPrint:
 	jmp -
 +
 	rts
-
-
-; characters 32 - 127
-TMS_FONT_DATA:
-!byte $00,$00,$00,$00,$00,$00,$00,$00 ; <SPACE>
-!byte $18,$3C,$3C,$18,$18,$00,$18,$00 ; !
-!byte $6C,$6C,$00,$00,$00,$00,$00,$00 ; "
-!byte $6C,$6C,$FE,$6C,$FE,$6C,$6C,$00 ; #
-!byte $18,$3E,$60,$3C,$06,$7C,$18,$00 ; $
-!byte $00,$C6,$CC,$18,$30,$66,$C6,$00 ; %
-!byte $38,$6C,$68,$76,$DC,$CC,$76,$00 ; &
-!byte $18,$18,$30,$00,$00,$00,$00,$00 ; '
-!byte $0C,$18,$30,$30,$30,$18,$0C,$00 ; (
-!byte $30,$18,$0C,$0C,$0C,$18,$30,$00 ; )
-!byte $00,$66,$3C,$FF,$3C,$66,$00,$00 ; *
-!byte $00,$18,$18,$7E,$18,$18,$00,$00 ; +
-!byte $00,$00,$00,$00,$00,$18,$18,$30 ; ,
-!byte $00,$00,$00,$7E,$00,$00,$00,$00 ; -
-!byte $00,$00,$00,$00,$00,$18,$18,$00 ; .
-!byte $03,$06,$0C,$18,$30,$60,$C0,$00 ; /
-!byte $3C,$66,$6E,$7E,$76,$66,$3C,$00 ; 0
-!byte $18,$38,$18,$18,$18,$18,$7E,$00 ; 1
-!byte $3C,$66,$06,$1C,$30,$66,$7E,$00 ; 2
-!byte $3C,$66,$06,$1C,$06,$66,$3C,$00 ; 3
-!byte $1C,$3C,$6C,$CC,$FE,$0C,$1E,$00 ; 4
-!byte $7E,$60,$7C,$06,$06,$66,$3C,$00 ; 5
-!byte $1C,$30,$60,$7C,$66,$66,$3C,$00 ; 6
-!byte $7E,$66,$06,$0C,$18,$18,$18,$00 ; 7
-!byte $3C,$66,$66,$3C,$66,$66,$3C,$00 ; 8
-!byte $3C,$66,$66,$3E,$06,$0C,$38,$00 ; 9
-!byte $00,$18,$18,$00,$00,$18,$18,$00 ; :
-!byte $00,$18,$18,$00,$00,$18,$18,$30 ; ;
-!byte $0C,$18,$30,$60,$30,$18,$0C,$00 ; <
-!byte $00,$00,$7E,$00,$00,$7E,$00,$00 ; =
-!byte $30,$18,$0C,$06,$0C,$18,$30,$00 ; >
-!byte $3C,$66,$06,$0C,$18,$00,$18,$00 ; ?
-!byte $7C,$C6,$DE,$DE,$DE,$C0,$78,$00 ; @
-!byte $18,$3C,$3C,$66,$7E,$C3,$C3,$00 ; A
-!byte $FC,$66,$66,$7C,$66,$66,$FC,$00 ; B
-!byte $3C,$66,$C0,$C0,$C0,$66,$3C,$00 ; C
-!byte $F8,$6C,$66,$66,$66,$6C,$F8,$00 ; D
-!byte $FE,$66,$60,$78,$60,$66,$FE,$00 ; E
-!byte $FE,$66,$60,$78,$60,$60,$F0,$00 ; F
-!byte $3C,$66,$C0,$CE,$C6,$66,$3E,$00 ; G
-!byte $66,$66,$66,$7E,$66,$66,$66,$00 ; H
-!byte $7E,$18,$18,$18,$18,$18,$7E,$00 ; I
-!byte $0E,$06,$06,$06,$66,$66,$3C,$00 ; J
-!byte $E6,$66,$6C,$78,$6C,$66,$E6,$00 ; K
-!byte $F0,$60,$60,$60,$62,$66,$FE,$00 ; L
-!byte $82,$C6,$EE,$FE,$D6,$C6,$C6,$00 ; M
-!byte $C6,$E6,$F6,$DE,$CE,$C6,$C6,$00 ; N
-!byte $38,$6C,$C6,$C6,$C6,$6C,$38,$00 ; O
-!byte $FC,$66,$66,$7C,$60,$60,$F0,$00 ; P
-!byte $38,$6C,$C6,$C6,$C6,$6C,$3C,$06 ; Q
-!byte $FC,$66,$66,$7C,$6C,$66,$E3,$00 ; R
-!byte $3C,$66,$70,$38,$0E,$66,$3C,$00 ; S
-!byte $7E,$5A,$18,$18,$18,$18,$3C,$00 ; T
-!byte $66,$66,$66,$66,$66,$66,$3E,$00 ; U
-!byte $C3,$C3,$66,$66,$3C,$3C,$18,$00 ; V
-!byte $C6,$C6,$C6,$D6,$FE,$EE,$C6,$00 ; W
-!byte $C3,$66,$3C,$18,$3C,$66,$C3,$00 ; X
-!byte $C3,$C3,$66,$3C,$18,$18,$3C,$00 ; Y
-!byte $FE,$C6,$8C,$18,$32,$66,$FE,$00 ; Z
-!byte $3C,$30,$30,$30,$30,$30,$3C,$00 ; [
-!byte $C0,$60,$30,$18,$0C,$06,$03,$00 ; \
-!byte $3C,$0C,$0C,$0C,$0C,$0C,$3C,$00 ; ]
-!byte $10,$38,$6C,$C6,$00,$00,$00,$00 ; ^
-!byte $00,$00,$00,$00,$00,$00,$00,$FE ; _
-!byte $18,$18,$0C,$00,$00,$00,$00,$00 ; `
-!byte $00,$00,$3C,$06,$1E,$66,$3B,$00 ; a
-!byte $E0,$60,$6C,$76,$66,$66,$3C,$00 ; b
-!byte $00,$00,$3C,$66,$60,$66,$3C,$00 ; c
-!byte $0E,$06,$36,$6E,$66,$66,$3B,$00 ; d
-!byte $00,$00,$3C,$66,$7E,$60,$3C,$00 ; e
-!byte $1C,$36,$30,$78,$30,$30,$78,$00 ; f
-!byte $00,$00,$3B,$66,$66,$3C,$C6,$7C ; g
-!byte $E0,$60,$6C,$76,$66,$66,$E6,$00 ; h
-!byte $18,$00,$38,$18,$18,$18,$3C,$00 ; i
-!byte $06,$00,$06,$06,$06,$06,$66,$3C ; j
-!byte $E0,$60,$66,$6C,$78,$6C,$E6,$00 ; k
-!byte $38,$18,$18,$18,$18,$18,$3C,$00 ; l
-!byte $00,$00,$66,$77,$6B,$63,$63,$00 ; m
-!byte $00,$00,$7C,$66,$66,$66,$66,$00 ; n
-!byte $00,$00,$3C,$66,$66,$66,$3C,$00 ; o
-!byte $00,$00,$DC,$66,$66,$7C,$60,$F0 ; p
-!byte $00,$00,$3D,$66,$66,$3E,$06,$07 ; q
-!byte $00,$00,$EC,$76,$66,$60,$F0,$00 ; r
-!byte $00,$00,$3E,$60,$3C,$06,$7C,$00 ; s
-!byte $10,$30,$7C,$30,$30,$34,$18,$00 ; t
-!byte $00,$00,$CC,$CC,$CC,$CC,$76,$00 ; u
-!byte $00,$00,$CC,$CC,$CC,$78,$30,$00 ; v
-!byte $00,$00,$C6,$D6,$D6,$6C,$6C,$00 ; w
-!byte $00,$00,$63,$36,$1C,$36,$63,$00 ; x
-!byte $00,$00,$66,$66,$66,$3C,$18,$70 ; y
-!byte $00,$00,$7E,$4C,$18,$32,$7E,$00 ; z
-!byte $0E,$18,$18,$70,$18,$18,$0E,$00 ; {
-!byte $18,$18,$18,$18,$18,$18,$18,$00 ; |
-!byte $70,$18,$18,$0E,$18,$18,$70,$00 ; }
-!byte $72,$9C,$00,$00,$00,$00,$00,$00 ; ~
-!byte $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff ;  
--- code/6502/tms9918test.asm
+++ code/6502/tms9918test.asm
@@ -3,13 +3,37 @@
 !source "hbc56.asm"
 
 !source "gfx/tms9918.asm"
+!source "gfx/fonts/tms9918font1.asm"
+
+sprite1:
+!byte %00001000
+!byte %00000100
+!byte %00001111
+!byte %00011011
+!byte %00111111
+!byte %00101111
+!byte %00101000
+!byte %00000110
+
+sprite2:
+!byte %00100000
+!byte %01000000
+!byte %11100000
+!byte %10110000
+!byte %11111000
+!byte %11101000
+!byte %00101000
+!byte %11000000
+
+XPOS = $44
+YPOS = $45
 
 main:
         lda #0
 
         jsr tmsInit
 
-        +tmsPrint "TI BASIC READY", 1, 16
+        +tmsPrint "TROY'S HBC-56 BASIC READY", 1, 15
         +tmsPrint ">10 PRINT \"HELLO WORLD!\"", 0, 17
         +tmsPrint ">RUN", 0, 18
         +tmsPrint "HELLO WORLD!", 1, 19
@@ -17,15 +41,74 @@ main:
         +tmsPrint ">", 0, 23
         +tmsPrint 127, 1, 23
 
-loop:
-        clc
-        adc #1
+        lda #40
+        sta XPOS
+        sta YPOS
 
-        jsr medDelay
+        +tmsCreateSpritePattern 0, sprite1
+        +tmsCreateSpritePattern 1, sprite2
+
+        +tmsCreateSprite 0, 0, 40, 40, TMS_MAGENTA
+        +tmsCreateSprite 1, 1, 56, 40, TMS_MAGENTA
+        +tmsCreateSprite 2, 0, 80, 60, TMS_MED_RED
+        +tmsCreateSprite 3, 1, 96, 60, TMS_MED_RED
+
+loop:
+
+	jsr delay
+        +tmsSpritePos 0, 42, 40
+        +tmsSpritePos 1, 58, 40
+        +tmsSpritePos 2, 82, 60
+        +tmsSpritePos 3, 98, 60
+	jsr delay
+        +tmsSpritePos 0, 44, 40
+        +tmsSpritePos 1, 60, 40
+        +tmsSpritePos 2, 84, 60
+        +tmsSpritePos 3, 100, 60
+	jsr delay
+        +tmsSpritePos 0, 46, 40
+        +tmsSpritePos 1, 62, 40
+        +tmsSpritePos 2, 86, 60
+        +tmsSpritePos 3, 102, 60
+	jsr delay
+        +tmsSpritePos 0, 48, 40
+        +tmsSpritePos 1, 64, 40
+        +tmsSpritePos 2, 88, 60
+        +tmsSpritePos 3, 104, 60
+	jsr delay
+        +tmsSpritePos 0, 50, 40
+        +tmsSpritePos 1, 66, 40
+        +tmsSpritePos 2, 88, 70
+        +tmsSpritePos 3, 104, 70
 
         +tmsPrint ' ', 1, 23
 
-        jsr medDelay
+	jsr delay
+        +tmsSpritePos 0, 48, 40
+        +tmsSpritePos 1, 64, 40
+        +tmsSpritePos 2, 86, 70
+        +tmsSpritePos 3, 102, 70
+	jsr delay
+        +tmsSpritePos 0, 46, 40
+        +tmsSpritePos 1, 62, 40
+        +tmsSpritePos 2, 84, 70
+        +tmsSpritePos 3, 100, 70
+	jsr delay
+        +tmsSpritePos 0, 44, 40
+        +tmsSpritePos 1, 60, 40
+        +tmsSpritePos 2, 82, 70
+        +tmsSpritePos 3, 98, 70
+	jsr delay
+        +tmsSpritePos 0, 42, 40
+        +tmsSpritePos 1, 58, 40
+        +tmsSpritePos 2, 80, 70
+        +tmsSpritePos 3, 96, 70
+	jsr delay
+        +tmsSpritePos 0, 40, 40
+        +tmsSpritePos 1, 56, 40
+        +tmsSpritePos 2, 80, 60
+        +tmsSpritePos 3, 96, 60
+
 
         +tmsPrint 127, 1, 23
 
