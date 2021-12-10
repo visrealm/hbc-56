dd089c47fdd54c3c00b0c6ad811eb5d483894e44
Troy Schrapel <troy.schrapel@petrosys.com.au>
2021-12-10 11:47:11 +1030

    Added missing macro files

---
+++ code/6502/kernel/kernel_macros.asm
@@ -0,0 +1,49 @@
+; 6502 - HBC-56 Kernel Macros
+;
+; Copyright (c) 2021 Troy Schrapel
+;
+; This code is licensed under the MIT license
+;
+; https://github.com/visrealm/hbc-56
+;
+;
+
+!macro setIntHandler .address {
+        lda #<.address
+        sta HBC56_INT_VECTOR + 1
+        lda #>.address
+        sta HBC56_INT_VECTOR + 2
+        lda #JMP_OPCODE
+        sta HBC56_INT_VECTOR
+}
+
+!macro setNmiHandler .address {
+        lda #<.address
+        sta HBC56_NMI_VECTOR + 1
+        lda #>.address
+        sta HBC56_NMI_VECTOR + 2
+        lda #JMP_OPCODE
+        sta HBC56_NMI_VECTOR
+}
+
+!macro setHbcMetaNES {
+        lda HBC56_CONSOLE_FLAGS
+        ora #HBC56_CONSOLE_FLAG_NES
+        sta HBC56_CONSOLE_FLAGS
+}
+
+!macro setHbcMetaTitle .titleStr {
+        jmp .hbcMetaTitleOut
+.titleStrLabel:
+        !text .titleStr
+.titleStrLabelLen = * - .titleStrLabel
+        !byte 0 ; nul terminator for game name
+
+.hbcMetaTitleOut:
+        +tmsPrintZ .titleStrLabel, (32 - .titleStrLabelLen) / 2, 15
+}
+
+!macro consoleEnableCursor {
+        lda #HBC56_CONSOLE_FLAG_CURSOR
+        sta HBC56_CONSOLE_FLAGS
+}
