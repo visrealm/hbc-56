0418048dd2451ff41bcde2258d621bad79d9415f
Troy Schrapel <troy.schrapel@petrosys.com.au>
2022-02-28 12:09:40 +1030

    Tools: Added hex output to img2strip.py

--- tools/img2strip.py
+++ tools/img2strip.py
@@ -18,16 +18,19 @@ TILE_SIZE  = 8
 for infile in sys.argv[1:]:
     f, e = os.path.splitext(infile)
     outfile = f + ".bin"
+    hexfile = f + ".hex"
     try:
         src = Image.open(infile)
         pix = src.load()
         dst = open(outfile,"wb")
+        dsth = open(hexfile,"w")
         
         inTilesX = int(src.width / TILE_SIZE)
         inTilesY = int(src.height / TILE_SIZE)
         numInTiles = inTilesX * inTilesY
 
         for y in range(inTilesY):
+          dsth.write("\n!byte ")
           for x in range(inTilesX):
             xOff = x * TILE_SIZE
             yOff = y * TILE_SIZE
@@ -41,8 +44,10 @@ for infile in sys.argv[1:]:
                 if col[0] + col[1] + col[2] < 200:
                     bv = bv | 1
               dst.write(bytes([bv & 0xff]))
+              dsth.write("$" + bytes([bv & 0xff]).hex() + ",")
 
         dst.close()
+        dsth.close()
         src.close()
     except IOError:
         print("cannot convert", infile)
