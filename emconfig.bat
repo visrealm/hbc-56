e41c76bd75364c8fc79cd8264c1c9ea205f8950b
Troy Schrapel <code@visualrealmsoftware.com>
2023-09-06 09:11:59 +0930

    Updated emconfig.bat to setup the build directory using emcmake

--- emconfig.bat
+++ emconfig.bat
@@ -1,6 +1,27 @@
+:: Setup WebAssembly build
+:: Takes a single argument bing the build directory (default: build_wasm)
+::
+:: Usage: 
+:: ./emconfig.bat <build_dir>
+::
+:: Example:
+:: ./emconfig.bat build_wasm
+
+
+@echo off
+setlocal
+set BUILD_DIR=build_wasm
+
+if "%~1" NEQ "" (
+  set "BUILD_DIR=%~1"
+)
+
 pushd "%~dp0"
 
 call tools\emsdk\emsdk.bat install 3.1.10
 call tools\emsdk\emsdk.bat activate 3.1.10
 
+emcmake cmake -B %BUILD_DIR% -S .
+
 popd
+endlocal
